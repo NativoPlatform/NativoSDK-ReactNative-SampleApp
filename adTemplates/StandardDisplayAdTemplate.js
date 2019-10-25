62fd7525ce31afbc291ca72535b9884a590b1dc8
nnavakiran <nnavakiran@nativo.com>
2019-10-25 14:23:05 -0600

    Resolve imports

--- adTemplates/StandardDisplayAdTemplate.js
+++ adTemplates/StandardDisplayAdTemplate.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Image, StyleSheet, Text, View} from "react-native";
+import React from 'react';
+import {StyleSheet, Text, View} from "react-native";
 import {WebView} from "react-native-webview";
 
 const StandardDisplayAdTemplate = (props) => {
