21ea17f2bfc9f35ea455b6bf0dbc5460f6ef350e
Matt Murray <mattm.trinsic@gmail.com>
2023-03-09 13:02:36 -0800

    New rn env v0.65.3

--- android/app/src/main/java/com/nativornsampleapp/MainActivity.java
+++ android/app/src/main/java/com/nativornsampleapp/MainActivity.java
@@ -1,27 +1,24 @@
-package com.reactsample;
+package com.nativornsampleapp;
 
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
-
 import net.nativo.sdk.NativoSDK;
 
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
     @Override
     protected String getMainComponentName() {
-        return "ReactSample";
+    return "NativoRNSampleApp";
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         NativoSDK.init(this);
     }
-
 }
